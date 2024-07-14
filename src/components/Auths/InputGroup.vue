4f74a8702e09b0d8ae657baf7e11cbca23940193
cristofferchoque <cristofferchoque@gmail.com>
2024-07-14 12:56:52 -0500

    refactor: Agrega funcion para capturar individualmente los estados de cada input mediante el componente global

--- src/components/Auths/InputGroup.vue
+++ src/components/Auths/InputGroup.vue
@@ -1,5 +1,9 @@
 <script setup>
-const props = defineProps(['label', 'type', 'placeholder'])
+const props = defineProps(['label', 'type', 'name', 'id', 'placeholder', 'extraClass'])
+
+import { useFormStore } from '@/stores/useFormStore'
+const formStore = useFormStore()
+const { formState } = formStore
 </script>
 
 <template>
@@ -8,8 +12,14 @@ const props = defineProps(['label', 'type', 'placeholder'])
     <div class="relative">
       <input
         :type="props.type"
+        :name="props.name"
+        :id="props.id"
         :placeholder="props.placeholder"
-        class="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary text-black dark:text-white"
+        v-model="formState[props.name]"
+        :class="[
+          'w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary text-black dark:text-white',
+          props.extraClass // Agrega la clase extra pasada por props si está definida
+        ]"
       />
 
       <span class="absolute right-4 top-4">
